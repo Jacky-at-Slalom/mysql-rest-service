4fe129faba9af2d4647fbdb8ffa8a71a82dac8c8
“Jacklyne <Jacklyne.Bouier@slalom.com>
2020-02-17 15:00:12 -0800

    initial commit

---
+++ src/main/java/com/javaproject/mysqlrestservice/MysqlRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.mysqlrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/mysqlrestservice/MysqlRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.mysqlrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
