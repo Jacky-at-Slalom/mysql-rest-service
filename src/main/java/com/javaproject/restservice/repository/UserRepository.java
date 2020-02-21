532db6b2b379d6ebd86d3c60a186ecd617ce0e44
“Jacklyne <Jacklyne.Bouier@slalom.com>
2020-02-20 16:58:39 -0800

    refactoring file structure and fix database bug

--- src/main/java/com/javaproject/restservice/repository/UserRepository.java
+++ src/main/java/com/javaproject/restservice/repository/UserRepository.java
@@ -1,11 +1,11 @@
-package  com.javaproject.restservice.repository;
+package com.javaproject.restservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.javaproject.restservice.entity.User;
 
-
-@RepositoryRestResource(path = "/users")
+@RepositoryRestResource(exported = false)
+//@RepositoryRestResource(path = "/users")
 public interface UserRepository extends JpaRepository<User, String>  {
 }
