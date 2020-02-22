20e3a05cf25dcffe72efaf35c12b26d55a330ab5
“Jacklyne <Jacklyne.Bouier@slalom.com>
2020-02-21 16:30:51 -0800

    Still work on unit test for findbyname

--- src/test/java/com/javaproject/mysqlrestservice/MysqlRestServiceApplicationTests.java
+++ src/test/java/com/javaproject/mysqlrestservice/MysqlRestServiceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.javaproject.mysqlrestservice;
 
+import com.javaproject.restservice.entity.*;
+import com.javaproject.restservice.repository.*;
+import org.junit.*;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.*;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.orm.jpa.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.*;
 
-@SpringBootTest
-class MysqlRestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+public class MysqlRestServiceApplicationTests {
+
+	 @RunWith(SpringRunner.class)
+	 @DataJpaTest
+	 public class UserRepositoryTest {
+
+
+		 @Autowired
+		 private UserRepository userRepository;
+
+
+		 @Test
+		 public void testFindByName() {
+			 Optional<User> user = UserRepository.find
+			 assertEquals("Foo", User.get().getName());
+		 }
+	 }
 
 }
+
