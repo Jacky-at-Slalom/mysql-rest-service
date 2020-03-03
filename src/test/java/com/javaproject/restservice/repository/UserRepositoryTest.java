387d5a65646f9781645a6d9973a7eba6774d423e
“Jacklyne <Jacklyne.Bouier@slalom.com>
2020-03-02 16:05:54 -0800

    added unit test for usercontroller findall user

--- src/test/java/com/javaproject/restservice/repository/UserRepositoryTest.java
+++ src/test/java/com/javaproject/restservice/repository/UserRepositoryTest.java
@@ -11,7 +11,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.*;
 import org.springframework.boot.test.mock.mockito.*;
+import org.springframework.boot.web.server.*;
 import org.springframework.data.repository.query.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
